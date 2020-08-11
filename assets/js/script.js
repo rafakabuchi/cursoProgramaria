42f09af2edf2f3979f69b5d2dce71784d54d3ce2
Rafaela Saori <rkabuchi@gmail.com>
2020-08-11 18:04:08 -0300

    Excelente curso básico de Front

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function validaFormulario() {
+    if (document.getElementById("nomeUser").value != "" && document.getElementById("emailUser").value != "" && document.getElementById("telUser").value != "") {
+        alert("Prontinho, você receberá as novidades por email")
+    } else {
+        alert("Mana, falta preencher dados!")
+    }
+}
+
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
